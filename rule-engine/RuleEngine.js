9ebac5bf7e3a9639ce3e381f8713a649b7fbaf92
XhitHub <com.trojan.exe@gmail.com>
2021-01-20 18:25:44 +0800

    re

---
+++ rule-engine/RuleEngine.js
@@ -0,0 +1,15 @@
+class RuleEngine {
+  constructor() {
+    // this._options = options
+    this.rules = []
+    this.gRules = []
+  }
+
+  chain(fact, connector) {
+    // find matching rules
+    this.rules.filter(rule => ru)
+  }
+}
+
+// This works
+module.exports = RuleEngine
